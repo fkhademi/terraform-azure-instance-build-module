6e45a1feaa625a2787c69c8cb5b4e9acc0f013a6
Frey Khademi <frey.khademi@gmail.com>
2020-12-21 22:35:23 +0100

    fix typo

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "pub_ip" {
   name                = "${var.name}-pub_ip"
   location            = var.region
   resource_group_name = var.rg
-  allocation_method   = "static"
+  allocation_method   = "Static"
 }
 resource "azurerm_network_interface" "nic" {
   name                = "${var.name}-nic"
