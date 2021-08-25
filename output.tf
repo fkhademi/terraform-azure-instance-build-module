9f3609dbce04a428baccb6c61ea5673a85aeaba8
Frey Khademi <frey.khademi@gmail.com>
2021-08-25 18:22:31 +0200

    added lan nic output

--- output.tf
+++ output.tf
@@ -8,6 +8,11 @@ output "nic" {
   value       = azurerm_network_interface.nic
 }
 
+output "lan_nic" {
+  description = "The created NIC as an object with all of it's attributes. This was created using the azurerm_network_interface resource."
+  value       = azurerm_network_interface.lan
+}
+
 output "public_ip" {
   description = "The public IP and all of it's attributes"
   value       = var.public_ip ? azurerm_public_ip.pub_ip[0] : null
